ef6b97a8d0c349b9785cb42c3c46cf1f86d64194
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2016-10-25 14:16:15 +0900

    better implementation of formatting

--- cssfmt.el
+++ cssfmt.el
@@ -1,12 +1,21 @@
 ;;; cssfmt.el --- Cssfmt interface
 ;; copyright (C) 2015 κeen All rights reserved.
-;; Use of this source code is governed by a BSD-style
-;; license that can be found in the LICENSE file.
 ;; Author: κeen
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Keywords: css code formatter
 ;; URL: https://github.com/KeenS/cssfmt.el
-;; This file is not part of GNU Emacs.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;; This is a thin wrapper of [cssfmt](https://github.com/morishitter/cssfmt)
@@ -23,16 +32,40 @@
   "'cssfmt' interface."
   :group 'css)
 
-(defcustom cssfmt-command "cssfmt"
+(defcustom cssfmt-command "stylefmt"
   "The 'cssfmt' command."
   :type 'string
   :group 'cssfmt)
 
-(defun cssfmt ()
+(defcustom cssfmt-popup-errors nil
+  "Display error buffer when cssfmt fails."
+  :type 'boolean)
+
+(defun cssfmt--call (buf)
+  "Format BUF using cssfmt."
+  (with-current-buffer (get-buffer-create "*cssfmt*")
+    (erase-buffer)
+    (insert-buffer-substring buf)
+    (if (zerop (call-process-region (point-min) (point-max) cssfmt-command t t nil))
+        (progn (copy-to-buffer buf (point-min) (point-max))
+               (kill-buffer))
+      (when cssfmt-popup-errors
+        (display-buffer (current-buffer)))
+      (error "cssfmt failed, see *cssfmt* buffer for details"))))
+
+;;;###autoload
+(defun cssfmt-format-buffer ()
   "Format the current buffer according to the cssfmt tool."
-  (save-excursion
-    (call-process cssfmt-command nil nil nil (buffer-file-name (current-buffer)))
-    (revert-buffer t t t)))
+  (interactive)
+  (unless (executable-find cssfmt-command)
+    (error "Could not locate executable \"%s\"" cssfmt-command))
+
+  (let ((cur-point (point))
+        (cur-win-start (window-start)))
+    (cssfmt--call (current-buffer))
+    (goto-char cur-point)
+    (set-window-start (selected-window) cur-win-start))
+  (message "Formatted buffer with cssfmt."))
 
 ;;;###autoload
 (defun cssfmt-enable-on-save ()
@@ -43,7 +76,7 @@ Note that this will cause css-mode to get loaded the first time
 you save any file, kind of defeating the point of autoloading."
 
   (interactive)
-  (add-hook 'after-save-hook 'cssfmt nil t))
+  (add-hook 'after-save-hook 'cssfmt-format-buffer nil t))
 
 (provide 'cssfmt)
 ;;; cssfmt.el ends here
